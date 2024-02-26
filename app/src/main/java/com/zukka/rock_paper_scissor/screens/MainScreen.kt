3687f7ba0ae970f3802918b73bc40b39b48d10f4
Zukka <zukka75@gmail.com>
2024-02-26 18:25:54 +0100

    Updated main screen

--- app/src/main/java/com/zukka/rock_paper_scissor/screens/MainScreen.kt
+++ app/src/main/java/com/zukka/rock_paper_scissor/screens/MainScreen.kt
@@ -1,8 +1,11 @@
 package com.zukka.rock_paper_scissor.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,9 +16,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.zukka.rock_paper_scissor.R
 import com.zukka.rock_paper_scissor.Screen
 
 
@@ -25,25 +32,44 @@ fun MainScreen(navController: NavController) {
     var numberOfPlayers by remember {
         mutableStateOf(1)
     }
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 50.dp)) {
-        Button(onClick = {
-            numberOfPlayers = 1
-            println(numberOfPlayers)
-            navController.navigate(Screen.GameScreen.withArgs(numberOfPlayers))
-        }) {
-            Text("One Player")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = {
-            numberOfPlayers = 2
-            println(numberOfPlayers)
-            navController.navigate(Screen.GameScreen.withArgs(numberOfPlayers))
-        }) {
-            Text("Two Players")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(
+            id = R.drawable.background),
+            contentDescription = "App background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+            )
+        Image(painter = painterResource(
+            id = R.drawable.title),
+            contentDescription = "App title",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(64.dp)
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 48.dp)
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = {
+                numberOfPlayers = 1
+                println(numberOfPlayers)
+                navController.navigate(Screen.GameScreen.withArgs(numberOfPlayers))
+            }) {
+                Text("One Player")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                numberOfPlayers = 2
+                println(numberOfPlayers)
+                navController.navigate(Screen.GameScreen.withArgs(numberOfPlayers))
+            }) {
+                Text("Two Players")
+            }
         }
     }
 }
